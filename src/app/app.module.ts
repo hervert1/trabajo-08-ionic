9237d4b13ff055bba82138d9d8fcbdbb0cdd5817
Armando Hervert <55454787+hervert1@users.noreply.github.com>
2020-11-28 01:56:27 -0600

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Insomnia } from '@ionic-native/insomnia/ngx';
+import { NavigationBar } from '@ionic-native/navigation-bar/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +19,11 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    Insomnia,
+    NavigationBar,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    
+
   ],
   bootstrap: [AppComponent]
 })
