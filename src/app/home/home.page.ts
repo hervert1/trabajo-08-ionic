925069f75fd8d008739de9a254ac5e4f6a0d2197
Armando Hervert <55454787+hervert1@users.noreply.github.com>
2020-11-28 02:27:44 -0600

    Actualizacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { clear } from 'console';
+//import { clear } from 'console';
 
 import { Insomnia } from '@ionic-native/insomnia/ngx';
 import { NavigationBar } from '@ionic-native/navigation-bar/ngx';
